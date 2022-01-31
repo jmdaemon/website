3bfb80a5e6df3f692402d2717888126e02afd99f
Joseph Diza <josephm.diza@gmail.com>
2022-01-30 22:13:28 -0800

    Fix embedded images

--- components/layout.tsx
+++ components/layout.tsx
@@ -32,7 +32,7 @@ export default function Layout({ children, home }: {children: React.ReactNode, h
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src={`${prefix}/images/profile.jpg`}
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -46,7 +46,7 @@ export default function Layout({ children, home }: {children: React.ReactNode, h
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src={`${prefix}/images/profile.jpg`}
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
