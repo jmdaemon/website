ace43224d39af60849f6ae08fffa3cb8419c52d2
Joseph Diza <josephm.diza@gmail.com>
2022-01-30 18:34:27 -0800

    Add header to first-post.js

--- pages/blog/posts/first-post.js
+++ pages/blog/posts/first-post.js
@@ -1,13 +1,16 @@
+import Head from 'next/head'
 import Link from 'next/link'
 export default function FirstPost() {
-    return (
-    <>
-      <h1>First Post</h1>
+  return (
+    <Layout>
+      <Head>
+        <h1>First Post</h1>
+      </Head>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
