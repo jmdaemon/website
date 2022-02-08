b221fd6f295436a17088db31df3dfd9a682b6a18
Joseph Diza <josephm.diza@gmail.com>
2022-02-07 19:25:44 -0800

    Add support for cv.pdf

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 import Layout, {siteTitle} from '../components/layout'
+import { prefix } from '../prefix'
 
 export default function Home() {
   return (
@@ -10,9 +11,8 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <Link href="/blog">
-          Blog
-        </Link>
+        <Link href="/blog"> Blog </Link>
+        <Link href={`${prefix}/cv.pdf`}> Resume </Link>
         <p>Hello I'm Joseph.
           I'm a free and open source software advocate, and love working for open source projects in my spare time.
         </p>
